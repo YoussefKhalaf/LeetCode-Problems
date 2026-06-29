fb21f8b7793bc34d71a5499912e65c839725de33
YoussefKhalaf <ykhalaf081@gmail.com>
2026-06-29 18:43:50 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,19 @@
 class Solution {
-  
+  bool isPalindrome(int x) {
+    if (x < 0) {
+      return false;
+    }
+    int reversed = 0;
+    int number = x;
+    while (number > 0) {
+      int digit = number % 10;
+      reversed = reversed * 10 + digit;
+      number ~/= 10;
+    }
+    if (x == reversed) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
