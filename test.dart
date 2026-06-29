b1f63dfe2cf5daa3e0e76cc68c7a18205c970e0c
YoussefKhalaf <ykhalaf081@gmail.com>
2026-06-29 18:03:53 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,14 @@
 class Solution {
-  
+  List<int> twoSum(List<int> nums, int target) {
+    Map<int, int> isExist = {};
+    for (int i = 0; i < nums.length; i++) {
+      int current = nums[i];
+      int second_number = target - current;
+      if (isExist.containsKey(second_number)) {
+        return [isExist[second_number]!, i];
+      }
+      isExist[current] = i;
+    }
+    return [];
+  }
 }
