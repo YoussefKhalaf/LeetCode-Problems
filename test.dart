f6033692f74d1e10b697ec0afcea5a423b5e30c3
YoussefKhalaf <ykhalaf081@gmail.com>
2026-06-30 11:39:49 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,12 @@
 class Solution {
-  
+  String longestCommonPrefix(List<String> strs) {
+    String commonPrefix = strs[0];
+    for (int i = 1; i < strs.length; i++) {
+      while (!strs[i].startsWith(commonPrefix)) {
+        commonPrefix = commonPrefix.substring(0, commonPrefix.length - 1);
+        if (commonPrefix.isEmpty) return "";
+      }
+    }
+    return commonPrefix;
+  }
 }
