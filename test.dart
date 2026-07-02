8758517fdf0f8218eb9c4281298e578090a87edf
YoussefKhalaf <ykhalaf081@gmail.com>
2026-07-02 17:29:35 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,24 @@
 class Solution {
-  
+  bool isValid(String s) {
+    List<String> stack = [];
+    Map<String, String> parentheses = {
+      '(': ')',
+      '{': '}',
+      '[': ']',
+    };
+    for (int i = 0; i < s.length; i++) {
+      String char = s[i];
+      if (parentheses.containsKey(char)) {
+        stack.add(char);
+      } else if (stack.isEmpty) {
+        return false;
+      } else {
+        String lastOne = stack.removeLast();
+        if (parentheses[lastOne] != char) {
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty;
+  }
 }
