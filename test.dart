13c5c729b782a59a141e9fc36787dfac3df63c7a
YoussefKhalaf <ykhalaf081@gmail.com>
2026-07-04 11:51:22 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
 class Solution {
-  
+  ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    ListNode result = ListNode(0);
+    ListNode current = result;
+    while (list1 != null && list2 != null) {
+      if (list1.val <= list2.val) {
+        current.next = list1;
+        list1 = list1.next;
+      } else {
+        current.next = list2;
+        list2 = list2.next;
+      }
+      current = current.next!;
+    }
+    if (list1 != null) {
+      current.next = list1;
+    } else {
+      current.next = list2;
+    }
+    return result.next;
+  }
 }
