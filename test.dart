b3ff19e8165d4644c4ea83075e20058839a36b15
YoussefKhalaf <ykhalaf081@gmail.com>
2026-07-04 12:47:18 +0300

    second solution

--- test.dart
+++ test.dart
@@ -1,15 +1,11 @@
 class Solution {
   int removeDuplicates(List<int> nums) {
-    List<int> unique = [];
-    int k = 0;
-    for (int i = 0; i < nums.length; i++) {
-      if (!unique.contains(nums[i])) {
-        unique.add(nums[i]);
+    int k = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] != nums[i - 1]) {
+        nums[k] = nums[i];
         k++;
       }
-    }
-    for (int i = 0; i < k; i++) {
-      nums[i] = unique[i];
     }
     return k;
   }
