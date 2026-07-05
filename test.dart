2e7fbae6a24cca72747846b27f9a33339be9d364
YoussefKhalaf <ykhalaf081@gmail.com>
2026-07-05 16:46:55 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,20 @@
 class Solution {
-  
+  List<int> plusOne(List<int> digits) {
+    int carry = 0;
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] != 9) {
+        carry = 0;
+        digits[i] += 1;
+        return digits;
+      } else {
+        digits[i] = 0;
+        carry = 1;
+      }
+    }
+    if (carry == 1) {
+      digits.insert(0, 1);
+      return digits;
+    }
+    return digits;
+  }
 }
