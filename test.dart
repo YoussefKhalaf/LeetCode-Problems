1dae675f71b937d900dff5c51e13db2e7016d236
YoussefKhalaf <ykhalaf081@gmail.com>
2026-07-05 11:34:39 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,13 @@
 class Solution {
-  
+  int strStr(String haystack, String needle) {
+    if (needle.length > haystack.length || !haystack.contains(needle)) {
+      return -1;
+    }
+    for (int i = 0; i <= haystack.length - needle.length; i++) {
+      if (haystack.substring(i, i + needle.length) == needle) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
