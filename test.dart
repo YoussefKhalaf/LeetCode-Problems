f1906eb9bdcca1c3b1a6fa3ef85d3c9d2b16a234
YoussefKhalaf <ykhalaf081@gmail.com>
2026-07-05 13:06:25 +0300

    first solution

--- test.dart
+++ test.dart
@@ -1,3 +1,16 @@
 class Solution {
-  
+  int lengthOfLastWord(String s) {
+    int index = 0;
+    s = s.trim();
+    for (int i = 0; i < s.length; i++) {
+      if (s[i] == " ") {
+        index = i;
+      }
+    }
+    if (index == 0) {
+      return s.length;
+    }
+    String res = s.substring(index + 1, s.length);
+    return res.length;
+  }
 }
